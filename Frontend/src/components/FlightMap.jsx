efbd6c3d6c600edf7b56938a31f37da91b47cd4e
Hannan Saleemi <hannan@octopuslabs.com>
2019-08-13 14:34:03 +0100

    Added some strcture to the react app - Header created with a logo and UTC Time in the top right - FlightMap has is own component yet to be styled

---
+++ Frontend/src/components/FlightMap.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+import "../assets/styles/_flightmap.scss";
+
+export default class FlightMap extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+          "position": [51.533128, 0.113951],
+          "flights": null
+        }
+    }
+
+    componentDidMount() {
+        fetch("http://0.0.0.0:5001/api/v1/flights/getAllFlightInfo")
+        .then(response => response.json())
+        .then(data => this.setState({
+          "flights": data
+        }))
+    }
+
+    render() {
+
+        var renderMarkers;
+        if(this.state.flights) {
+            renderMarkers = this.state.flights.map((flight) => {
+                console.log(flight)
+                return (
+                <Marker position={[flight.Lat, flight.Lon]}>
+                    <Popup>
+                    Flight: {flight.Flight}
+                    <br/>
+                    ICAO: {flight.ICAO}
+                    </Popup>
+                </Marker>
+                )
+            })
+        }
+
+        return (
+            <div className="leaflet-container">
+
+                <LeafletMap
+                center={this.state.position}
+                zoom={10}
+                maxZoom={18}
+                attributionControl={true}
+                zoomControl={true}
+                doubleClickZoom={false}
+                scrollWheelZoom={false}
+                dragging={true}
+                animate={true}
+                easeLinearity={0.35}
+                >
+
+                <TileLayer
+                    attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                />
+
+                {renderMarkers}
+
+                </LeafletMap>
+            </div>
+        )
+    }
+
+}
---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "../assets/styles/_header.scss";
+
+export default class Header extends Component {
+
+    getCurrentUTCTime = () => {
+        var date = new Date();
+        return `${date.getUTCHours()}:${date.getUTCMinutes()}`;
+    };
+
+    render() {
+
+        return (
+            <div className="site-nav">
+                <header className="main-header">
+                    <div className="custom-container">
+                        <div className="logo-wrap">
+                            <div className="logo">
+                                WhoIsAboveMe
+                            </div>
+                        </div>
+
+                        <div className="float-r">
+                            <div className="header-utc-time">
+                                UTC {this.getCurrentUTCTime()}
+                            </div>
+                        </div>
+                    </div>
+                </header>
+            </div>
+        )
+    }
+
+}
