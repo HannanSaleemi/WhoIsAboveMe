9cee7ee4aa2f9c5def26724ef6a5e28810c17a9e
Hannan Saleemi <hannan@octopuslabs.com>
2019-08-19 15:42:39 +0100

    Confirmed Queue of tasks working as required and successful result

--- Backend/main.py
+++ Backend/main.py
@@ -204,5 +204,5 @@ def index():
     return jsonify(response)
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
